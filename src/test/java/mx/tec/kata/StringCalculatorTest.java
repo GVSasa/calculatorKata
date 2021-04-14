f976ddcb41ba10ca71f7004ea6ebdabea5b88ade
GVSasa <Sebastiancitodelasierra@hotmail.com>
2021-04-14 14:50:24 -0600

    Challenge 2

--- src/test/java/mx/tec/kata/StringCalculatorTest.java
+++ src/test/java/mx/tec/kata/StringCalculatorTest.java
@@ -40,5 +40,12 @@ class StringCalculatorTest {
 		
 	}
 	
-	
+	void testGivenCalculatorWhenAdd1_3_5_7_9Then25 () {
+		int expectedResult = 25;
+		String numbers = "1,3,5,7,9";
+		
+		int actualResult = calculator.add(numbers);
+		assertEquals(expectedResult, actualResult);
+		
+	}
 }
